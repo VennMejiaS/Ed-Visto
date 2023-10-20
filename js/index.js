1174e2487a62c46c6995a3d8d5e4e17e98ea5ed9
VennMejiaS <125710563+VennMejiaS@users.noreply.github.com>
2023-10-19 23:51:23 -0500

    se agrega la función del dictado por voz

--- js/index.js
+++ js/index.js
@@ -1,3 +1,91 @@
+// Variable para indicar si el reconocimiento está activado
+let recognitionActiveTitle = false;
+let recognitionActiveImportant = false;
+let recognitionTitle; // Declarar la variable fuera del bloque condicional
+let recognitionImportant; // Declarar la variable fuera del bloque condicional
+
+// Función para verificar si el navegador es compatible
+const elNavegadorEsCompatible = () => {
+    if (navigator.userAgent.indexOf("Chrome") !== -1 ||  
+        navigator.userAgent.indexOf("Edge") !== -1 ||  
+        navigator.userAgent.indexOf("Safari") !== -1) {
+        return true;
+    }
+    alert('El Navegador no es compatible con el Reconocimiento de voz');
+    return false;
+};
+
+ // Función para iniciar o detener el reconocimiento al hacer clic en el botón
+ const toggleRecognitionTitle = () => {
+     if (recognitionActiveTitle) {
+         recognitionTitle.stop();
+         document.getElementById("voice-title").innerText = "Dictado desactivado";
+     } else {
+         recognitionTitle.start();
+         document.getElementById("voice-title").innerText = "Dictado activado";
+     }
+     recognitionActiveTitle = !recognitionActiveTitle;
+ };
+
+ const toggleRecognitionImportant = () => {
+     if (recognitionActiveImportant) {
+         recognitionImportant.stop();
+         document.getElementById("voice-important").innerText = "Dictado desactivado";
+     } else {
+         recognitionImportant.start();
+         document.getElementById("voice-important").innerText = "Dictado activado";
+     }
+     recognitionActiveImportant = !recognitionActiveImportant;
+ };
+
+ document.addEventListener("DOMContentLoaded", function() {
+
+     // Si el navegador es compatible, configurar el reconocimiento de voz para el título
+     if (elNavegadorEsCompatible()) {
+        recognitionTitle = new (window.SpeechRecognition || window.webkitSpeechRecognition || 
+                                 window.mozSpeechRecognition || window.msSpeechRecognition)();
+         recognitionTitle.lang = "es-US";
+
+         recognitionTitle.onend = (event) => { 
+             if (recognitionActiveTitle) {
+                 recognitionTitle.start(); 
+             }
+         };
+
+         recognitionTitle.onresult = (resultado) => { manejarResultadoTitle(resultado); }; 
+
+         document.getElementById("voice-title").addEventListener("click", toggleRecognitionTitle);
+     }
+
+     // Si el navegador es compatible, configurar el reconocimiento de voz para el texto importante
+     if (elNavegadorEsCompatible()) {
+        recognitionImportant = new (window.SpeechRecognition || window.webkitSpeechRecognition || 
+                                     window.mozSpeechRecognition || window.msSpeechRecognition)();
+         recognitionImportant.lang = "es-US";
+
+         recognitionImportant.onend = (event) => { 
+             if (recognitionActiveImportant) {
+                 recognitionImportant.start(); 
+             }
+         };
+
+         recognitionImportant.onresult = resultado => { manejarResultadoImportant(resultado); }; 
+
+         document.getElementById("voice-important").addEventListener("click", toggleRecognitionImportant);
+     }
+ });
+
+ // Función que maneja el resultado del reconocimiento de voz para el título
+ const manejarResultadoTitle = resultado => {
+     document.getElementById("titulo").value = resultado.results[0][0].transcript;
+ };
+
+ // Función que maneja el resultado del reconocimiento de voz para el texto importante
+ const manejarResultadoImportant = resultado => {
+     document.getElementById("important").value = resultado.results[0][0].transcript;
+ };
+
+
 let title = document.getElementById ('titulo')
 console.log(title)
 let tituloLength = document.getElementById ('titulo-length')
@@ -15,58 +103,4 @@ let inputLenght = introduction.value.length
 importantLength.innerText = `${inputLenght}/5000`
 }
 )
-
-let climax = document.getElementById ('amazing')
-console.log(climax)
-let amazingLength = document.getElementById ('amazing-length')
-climax.addEventListener ('input', function (event) {
-let inputLenght = climax.value.length
-amazingLength.innerText = `${inputLenght}/5000`
-}
-)
-
-let know = document.getElementById ('knowledge')
-console.log(know)
-let knowledgeLength = document.getElementById ('knowledge-length')
-know.addEventListener ('input', function (event) {
-let inputLenght = know.value.length
-knowledgeLength.innerText = `${inputLenght}/5000`
-}
-)
-
-let end = document.getElementById ('learning')
-console.log(end)
-let learningLength = document.getElementById ('learning-length')
-end.addEventListener ('input', function (event) {
-let inputLenght = end.value.length
-learningLength.innerText = `${inputLenght}/5000`
-}
-)
-
-let source = document.getElementById ('bibliography')
-console.log(source)
-let bibliographyLength = document.getElementById ('bibliography-length')
-source.addEventListener ('input', function (event) {
-let inputLenght = source.value.length
-bibliographyLength.innerText = `${inputLenght}/5000`
-}
-)
-
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("form");
-
-    form.addEventListener("submit", function(event) {
-        const formElements = form.querySelectorAll("input, textarea");
-
-        formElements.forEach(function(element) {
-            if (element.required && element.value.trim() === "") {
-                event.preventDefault(); // Evitar el envío del formulario
-                const fieldName = element.id;
-                const fieldLabel = element.previousElementSibling.textContent;
-                alert(`${fieldLabel} es un campo obligatorio. Por favor, completarlo.`);
-            }
-        });
-    });
-});
-
 
